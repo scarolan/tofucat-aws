13e251abcf9a816e2c957ddbc6545647b8ae45b3
Sean Carolan <sean@carolan.io>
2024-03-16 10:52:06 -0400

    Fix placeholder links.

--- variables.tf
+++ variables.tf
@@ -35,16 +35,16 @@ variable "admin_username" {
 }
 
 variable "height" {
-  default     = "401"
+  default     = "399"
   description = "Image height in pixels."
 }
 
 variable "width" {
-  default     = "601"
+  default     = "599"
   description = "Image width in pixels."
 }
 
 variable "placeholder" {
-  default     = "placekitten.com"
+  default     = "loremflickr.com"
   description = "Image-as-a-service URL. Some other fun ones to try are fillmurray.com, placecage.com, placebeard.it, loremflickr.com, baconmockup.com, placeimg.com, placebear.com, placeskull.com, stevensegallery.com, placedog.net"
 }
